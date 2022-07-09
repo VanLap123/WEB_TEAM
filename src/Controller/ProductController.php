c61a4ebb9a27163a6e81f43f4d2f032848eb93b4
Dann2922 <105551885+Dann2922@users.noreply.github.com>
2022-07-10 01:43:03 +0700

    Update ProductController.php
    Remove editProductAction

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -60,39 +60,7 @@ class ProductController extends AbstractController
       'form' => $productForm->createView()
     ]);
   }
-      /**
-     * @Route("/edit_pro/{id}", name="editProduct")
-     */
-    public function editProductAction(ManagerRegistry $res, Request $req, ValidatorInterface $valid, ProductRepository $repo, $id): Response
-    {
-      $product = $repo->find($id);
-      $productForm =$this->createForm(ProductType::class, $product);
-
-      $productForm->handleRequest($req);
-      $entity = $res->getManager();
-
-      if ($productForm->isSubmitted() && $productForm->isValid()) {
-          $data = $productForm->getData();
-          $product->setProName($data->getProName());
-          $product->setPrice($data-> getPrice());
-          $product->setOldPrice($data->getOldPrice());
-          $product->setProDesc($data-> getProDesc());
-          $product->setProQty($data->getProQty());
-          $product->setProImage($data->getProImage());
-          $product->setCat($data->getCat());
-          $err = $valid->validate($product);
-          if (count($err) > 0) {
-            $string_err = (string)$err;
-            return new Response($string_err, 400);
-          }
-          $entity->persist($product);
-          $entity->flush();
-          return $this->redirectToRoute("car_index");
-        }
-        return $this->render('product/Add_Pro.html.twig', [
-          'form' => $productForm->createView()
-        ]);
-    }
+      
       /**
      * @Route("/delete_pro/{id}", name="deleteProduct")
      */

d5c859c9adb06c3fcfa6731c5f86faa6bc69b080
Ho Kien Vinh <93614132+OwenHo198@users.noreply.github.com>
2022-07-10 12:12:36 +0700

    Update HomeController.php
    Update HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,11 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="app_home")
      */
-    public function index(): Response
+    public function index(ProductRepository $repo): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        $product = $repo->showAllProduct();
+        return $this->render('view/content.html.twig',[
+            'product'=> $product
         ]);
     }
 }
